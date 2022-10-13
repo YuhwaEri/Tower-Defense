96c471a294379fa23338a63691cb94c71a026522
tou <tou@email.arizona.edu>
2022-10-13 16:14:19 -0700

    Added a boolean in TowerDefenseView.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,7 @@ module com.group7 {
     opens com.group7 to javafx.fxml;
     exports com.group7;
     exports com.group7.view;
+    // Remove the comments when folder contains files.
+    //exports com.group7.model;
+    //exports com.group7.controller;
 }
